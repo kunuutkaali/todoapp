85ea02952727ff0f032d2716b35f0c169c1d0253
jon2502 <jonbjarke25@outlook.dk>
2023-02-14 15:02:09 +0100

    opdatering af route

--- routes/todos.js
+++ routes/todos.js
@@ -13,9 +13,23 @@ route.post('/new', (req, res)=>{
     res.render('todos/new')
 })
 
-route.post('/todos', async function(req, res, next){
-    controller.postTodos(req, res, next);
-    res.send('create todo')
+route.post('/todos', (req, res) => {
+    const user = new User({
+        Titel: req.body.Titel,
+        Description: req.body.Description,
+        Starting_date: req.body.Starting_date,
+        End_date: req.body.password
+    })
+    user.save((err) =>{
+        if(err) {
+            res.render('/todos', {
+                todo: todo,
+                errorMessage: "Error creating username, try another user"
+            })
+        } else {
+            res.redirect('/todos')
+        }
+    })
 })
 
 
