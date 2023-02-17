9be115cc281554d998e55823e503f82566ead951
Kunuut Kaali <kunuutkaali@gmail.com>
2023-02-17 02:13:35 +0100

    Ability to change done status

--- routes/todos.js
+++ routes/todos.js
@@ -2,23 +2,23 @@
 const route = require('express').Router();// All user gets posts Deletes goes here
 const Todo = require('../models/todo')
 const User = require('../models/user')
-const db = require('../db')
+require('../db')
 
 route.get('/', async(req, res)=>{
-    const foundUser = await User.findOne({username: req.user.username})
+    const foundUser = await User.find({username: req.user.username})
     if(foundUser){
         let now = new Date()
-        const active = await Todo.find({user:foundUser.id, deadline: { $gt: now }, startDate: { $lt: now }}).sort({ deadline: -1 })
-        const future = await Todo.find({user:foundUser.id, deadline: { $gt: now }, startDate: { $gt: now }}).sort({ deadline: -1 })
-        const dead = await Todo.find({user:foundUser.id, deadline: { $lt: now }})
+        const active = await Todo.find({user:foundUser, deadline: { $gt: now }, startDate: { $lt: now }})
+        const future = await Todo.find({user:foundUser, deadline: { $gt: now }, startDate: { $gt: now }})
+        const dead = await Todo.find({user:foundUser, deadline: { $lt: now }})
         if(
             active.length == 0 && 
             dead.length == 0 &&
             future.length == 0
             ){
-            res.render('todos/index', {errorMessage: "No todos"})
+            res.render('todos/index', {errorMessage: "No todos", user:req.user})
         }else{
-            res.render('todos/index', {active: active, dead:dead, future:future, user:foundUser.username})
+            res.render('todos/index', {active: active, dead:dead, future:future, user:req.user})
         }
     }else{
         // Finding user in db error
@@ -28,8 +28,7 @@ route.get('/', async(req, res)=>{
 
 // Create new todo form
 route.get('/new', async(req, res)=>{
-    const foundUser = await User.findOne({username: req.user.username})
-    res.render('todos/new', {user:foundUser.username})
+    res.render('todos/new', {user:req.user})
 })
 
 // Create new todo post req
@@ -37,14 +36,13 @@ route.post('/new', async (req, res)=>{
     // Validering
     try {
         const foundUser = await User.findOne({username: req.user.username})
-
         const todo = new Todo({
             title: req.body.title,
             description: req.body.description,
             startDate: req.body.startDate,
             deadline: req.body.deadline,
             done: req.body.done,
-            user: foundUser.id
+            user: foundUser
         })
         try {
             await todo.save()
@@ -57,7 +55,22 @@ route.post('/new', async (req, res)=>{
             res.render('todos/new', {errorMessage: error, todo, user:req.user.username})
         }
     } catch (error) {
+
         console.error(error);
+    }
+})
+// Update todo status to done
+route.post('/todo', async (req, res)=>{
+    const foundUser = await User.find({username: req.user.username})
+    const query = {_id:req.body.id}
+    const foundTodo = await Todo.findOne(query)
+    if(foundTodo.done == false){
+        await Todo.findOneAndUpdate(query, { $set: { done: true }})
+        res.redirect("/todos")
+    }else{
+        if(await Todo.findOneAndUpdate(query, { $set: { done: false }})){
+            res.redirect("/todos") 
+        }
     }
 })
 
